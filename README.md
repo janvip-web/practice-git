db8e8d740b123b789231a1fdba5ee1e559312f5b
janvip-web <janvip@webelight.co.in>
2026-01-02 15:45:58 +0530

    mdified files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # practice-git
 Gaining information about basic git and commands
+using PyCharm code editor
--- python_demo.py
+++ python_demo.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 name = "Janvi Parmar"
-a1 = np.array([1,3,5,7,9,11,13,15})
+a1 = np.array([1,3,5,7,9,11,13,15])
 mean = np.mean(a1)
 
 print("Name of Author:", name)
