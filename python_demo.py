02d89226f86ac8e329354ef95a8a2a00598c78b9
janvip-web <janvip@webelight.co.in>
2026-01-05 15:20:21 +0530

    stash commit

--- python_demo.py
+++ python_demo.py
@@ -1,3 +1,40 @@
+print("Hello!!,  from python-oop branch")
+print("Object oriented programming is contain class and objects")
+
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * self.radius
+
+    def perimeter(self):
+        return self.radius * self.radius * 2
+
+print("calling circle object")
+c = Circle(5)
+print("Area of circle",c.area())
+print("Perimeter of circle",c.perimeter())
+
+
+
+
 import numpy as np
 
 name = "Janvi Parmar"
@@ -6,8 +43,3 @@ mean = np.mean(a1)
 
 print("Name of Author:", name)
 print("Mean of given array:",mean)
-
-# data types :
-age = 18 #int
-sum = 124.535 #float
-name = "Janvi Parmar" #string etc...
