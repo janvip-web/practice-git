5da3f79e50d47abd8600b40f705b2f49eaa087b4
janvip-web <janvip@webelight.co.in>
2026-01-05 10:35:26 +0530

    update oop

--- python_demo.py
+++ python_demo.py
@@ -1,3 +1,39 @@
+print("Object oriented programming is contain class and objects")
+
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * self.radius
+
+    def perimeter(self):
+        return self.radius * self.radius * 2
+
+print("calling circle object")
+c = Circle(5)
+print("Area of circle",c.area())
+print("Perimeter of circle",c.perimeter())
+
+
+
+
 import numpy as np
 
 name = "Janvi Parmar"
